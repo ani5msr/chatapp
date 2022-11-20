dd43724b6e5d130916f2e0b26df3679363618ade
Anirudh Mishra <anirudh5mar@gmail.com>
2022-11-21 00:49:42 +0530

    Added views index and layout

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
+var partials = require('express-partials');
 var app = express();
 var errorHandlers = require('./middleware/errorhandler');
 var log = require('./middleware/log');
+app.use(partials());
+app.set('view options', {defaultLayout: 'layout'})
 app.use(log.logger);
 app.use(express.static(__dirname + '/static'));
 var routes = require('./routes');
+app.set('view engine', 'ejs');
+app.use(log.logger);
 app.get('/', routes.index);
 app.get('/login', routes.login);
 app.post('/login', routes.loginProcess);
 app.get('/chat', routes.chat);
 app.get('/account/login', routes.login);
+app.get('/error', function(req, res, next){
+    next(new Error('A contrived error'));
+   });
+app.use(errorHandlers.error);
 app.use(errorHandlers.notFound);
 app.listen(3000);
 console.log("App server running on port 3000");
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ module.exports.index = index;
 module.exports.login = login;
 module.exports.loginProcess = loginProcess;
 module.exports.chat = chat;
-function index(req, res){
- res.send('Index');
-};
+exports.index = function index(req, res){
+    res.render('index', {layout: 'layout', title: 'Index'});
+   };
 function login(req, res){
- res.send('Login');
+    res.render('login', {title: 'Login'});
 };
 function loginProcess(req, res){
  res.redirect('/');
 };
 function chat(req, res){
- res.send('Chat');
+ res.render('chat', {title: 'Chat'});
 };
